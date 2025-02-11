467295165c336878fb8fb3541644091268f5e73f
SanjayHeerani <lohanasanjay460@gmail.com>
2025-02-11 20:21:02 +0500

    AI ChatBot Backend Spring Boot

---
+++ src/main/java/com/sanjay/MyContrioller.java
@@ -0,0 +1,23 @@
+package com.sanjay;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api")
+public class MyContrioller {
+
+
+    private final QnAService qnAService;
+
+    @PostMapping("/chat")
+    public ResponseEntity<String> askQuestion(@RequestBody Map<String, String> payload){
+        String question = payload.get("question");
+        String answer = qnAService.getAnswer(question);
+        return ResponseEntity.ok(answer);
+    }
+}
